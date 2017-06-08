10a280a81b8bac2eb19656c3cfaffc8933273315
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-06-08 22:26:12 +0400

    micro-service-nutshell

--- service-discovery/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/api/gateway/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/api/gateway/ServiceDiscoveryApplication.java
@@ -2,22 +2,14 @@ package io.github.devbhuwan.microservices.nutshell.order.service.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Bhuwan Upadhyay
  * @date 2017/06/08
  */
+@EnableEurekaServer
 @SpringBootApplication
-@EnableZuulProxy
-@EnableEurekaClient
-@EnableFeignClients(basePackages = {"io.github.devbhuwan"})
-@EnableCircuitBreaker
-@Configuration
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
