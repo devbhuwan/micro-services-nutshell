ab02a7d0fdcaf75c26b4720a6afee736e44c8231
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-06-09 13:20:55 +0400

    micro-service-nutshell

--- configuration/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/configuration/ServiceConfigurationApplication.java
+++ configuration/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/configuration/ServiceConfigurationApplication.java
@@ -3,12 +3,14 @@ package io.github.devbhuwan.microservices.nutshell.order.service.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Bhuwan Upadhyay
  * @date 2017/06/08
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceConfigurationApplication {
 
