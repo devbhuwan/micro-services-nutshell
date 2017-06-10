25ab626f3637652f9272e2e6c36875bc413e0fc5
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-06-10 15:39:38 +0400

    hystrix dashboard

--- service-hystrix-dashboard/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/hystrix/ServiceHystrixDashboardApplication.java
+++ service-hystrix-dashboard/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/hystrix/ServiceHystrixDashboardApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author Bhuwan Upadhyay
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableDiscoveryClient
+@EnableTurbine
 public class ServiceHystrixDashboardApplication {
 
     public static void main(String[] args) {
