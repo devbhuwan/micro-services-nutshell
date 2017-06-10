4b49f497f1400b93f823a280fd516759809aa609
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-06-10 15:20:26 +0400

    zuul proxy in api gateway

--- configuration/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/configuration/ServiceConfigurationApplication.java
+++ configuration/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/configuration/ServiceConfigurationApplication.java
@@ -2,6 +2,7 @@ package io.github.devbhuwan.microservices.nutshell.order.service.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -9,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author Bhuwan Upadhyay
  * @date 2017/06/08
  */
-@EnableConfigServer
-@EnableEurekaClient
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ServiceConfigurationApplication {
 
     public static void main(String[] args) {
