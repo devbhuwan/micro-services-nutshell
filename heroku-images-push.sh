69b6fdc6f7eb86f4d18723890c407681965920b2
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-08 12:22:19 +0400

    logging

--- heroku-images-push.sh
+++ heroku-images-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #heroku container:login
-for X in configuration service-discovery order-service-runner service-api-gateway service-auth-server service-hystrix-dashboard
+for X in configuration service-registry order-service-runner service-api-gateway service-auth-server service-hystrix-dashboard
 do
     heroku apps:destroy --app --confirm oms-${X}
     heroku apps:create oms-${X}
