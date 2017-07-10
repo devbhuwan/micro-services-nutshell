7aee9a9bd087f794a7c10fcba8c3106d3a8d8f3a
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-10 22:54:37 +0400

    service logging and monitoring

--- order-service/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/OrderServiceApplication.java
+++ order-service/src/main/java/io/github/devbhuwan/microservices/nutshell/order/service/OrderServiceApplication.java
@@ -2,12 +2,15 @@ package io.github.devbhuwan.microservices.nutshell.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Bhuwan Upadhyay
  * @date 2017/07/10
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
