f8c7d585f2e9da942ed6b7a4a90f91644d9b2ebf
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-10 23:14:54 +0400

    service logging and monitoring

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 X_CURRENT_PATH=$(pwd)
 
-for X in configuration service-registry order-service-runner service-api-gateway service-auth-server service-hystrix-dashboard
+for X in configuration service-registry order-service service-api-gateway service-auth-server service-hystrix-dashboard
 do
     cd ${X_CURRENT_PATH}/${X}
     gradle clean build buildDocker
