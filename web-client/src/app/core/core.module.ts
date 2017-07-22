edbb911d34c7b353dad38c02cc8bd719b85dffc5
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-22 15:50:40 +0400

    POC-micro services

---
+++ web-client/src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkflowComponent } from './workflow/workflow.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WorkflowComponent]
+})
+export class CoreModule { }
---
+++ web-client/src/app/core/workflow/workflow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oms-workflow',
+  templateUrl: './workflow.component.html',
+  styleUrls: ['./workflow.component.scss']
+})
+export class WorkflowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
