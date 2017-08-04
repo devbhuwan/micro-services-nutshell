c3acdb35de1db0abb029a54b096e79c0c0a3a650
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-04 09:56:59 +0400

    Cammunda Workflow + Security Integration

--- micro-service-security-starter/src/main/java/io/github/devbhuwan/microservices/security/autoconfigure/MicroServiceSecurityAutoConfiguration.java
+++ micro-service-security-starter/src/main/java/io/github/devbhuwan/microservices/security/autoconfigure/MicroServiceSecurityAutoConfiguration.java
@@ -1,12 +1,17 @@
 package io.github.devbhuwan.microservices.security.autoconfigure;
 
+import io.github.devbhuwan.microservices.security.filters.BeforeWorkflowApiServiceRequestFilter;
+import io.github.devbhuwan.microservices.security.providers.UserAuthenticationProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.annotation.PostConstruct;
 
@@ -22,10 +27,26 @@ public class MicroServiceSecurityAutoConfiguration extends WebSecurityConfigurer
     @Autowired
     private MicroServiceSecurityProperties microServiceSecurityProperties;
 
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private UserAuthenticationProvider userAuthenticationProvider;
+
     @PostConstruct
     public void beforeConstruct() {
         log.info("START -> " + this.getClass().getName());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new BeforeWorkflowApiServiceRequestFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/**");
+
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(userAuthenticationProvider);
+    }
 
 }
---
+++ micro-service-security-starter/src/main/java/io/github/devbhuwan/microservices/security/providers/UserAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package io.github.devbhuwan.microservices.security.providers;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+
+}
