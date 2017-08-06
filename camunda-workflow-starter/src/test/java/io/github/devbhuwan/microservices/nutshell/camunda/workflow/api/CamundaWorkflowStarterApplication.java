00aa50ac108a9453b629ef3662e0bab0d4917fec
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-06 22:06:48 +0400

    Cammunda Workflow + Security Integration

--- camunda-workflow-starter/src/test/java/io/github/devbhuwan/microservices/nutshell/camunda/workflow/api/CamundaWorkflowStarterApplication.java
+++ camunda-workflow-starter/src/test/java/io/github/devbhuwan/microservices/nutshell/camunda/workflow/api/CamundaWorkflowStarterApplication.java
@@ -3,18 +3,37 @@ package io.github.devbhuwan.microservices.nutshell.camunda.workflow.api;
 
 import io.github.devbhuwan.microservices.nutshell.camunda.workflow.autoconfigure.CamundaWorkflowAutoConfiguration;
 import io.github.devbhuwan.microservices.security.autoconfigure.MicroServiceSecurityAutoConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.spring.boot.starter.event.ProcessApplicationStartedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
+import org.springframework.util.Assert;
 
 @SpringBootApplication
 @ComponentScan(value = {
         MicroServiceSecurityAutoConfiguration.BASE_PACKAGE,
         CamundaWorkflowAutoConfiguration.BASE_PACKAGE
 })
+@Slf4j
 public class CamundaWorkflowStarterApplication {
+
+    public static final String WF_REQUEST_APPROVAL = "wf-request-approval";
+    @Autowired
+    private RepositoryService repositoryService;
 
     public static void main(String[] args) {
         SpringApplication.run(CamundaWorkflowStarterApplication.class, args);
+    }
+
+    @EventListener
+    public void notify(final ProcessApplicationStartedEvent unused) {
+        final ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().processDefinitionKey(WF_REQUEST_APPROVAL).singleResult();
+        log.info("Found deployed process: {}", processDefinition);
+        //Assert.notNull(processDefinition, "process '" + WF_REQUEST_APPROVAL + "' should be deployed!");
     }
 }
--- camunda-workflow-starter/src/test/java/io/github/devbhuwan/microservices/nutshell/camunda/workflow/api/RESTWorkflowApiServiceTest.java
+++ camunda-workflow-starter/src/test/java/io/github/devbhuwan/microservices/nutshell/camunda/workflow/api/RESTWorkflowApiServiceTest.java
@@ -1,17 +1,29 @@
 package io.github.devbhuwan.microservices.nutshell.camunda.workflow.api;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import static org.junit.Assert.assertNotNull;
+
 public class RESTWorkflowApiServiceTest extends AbstractTest {
 
+    public static final String WF_REQUEST_APPROVAL = "wf-request-approval";
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
 
     @Test
-    public void requesterApporvalWorkflow() {
-
+    public void requesterApporvalWorkflow() throws InterruptedException {
+        Thread.sleep(5000);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(WF_REQUEST_APPROVAL);
+        assertNotNull(processInstance);
     }
 
 }
