aef72f5f68391d8d66f4482da3cbdbbc760dfda2
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-07 21:32:00 +0400

    bpm-service

--- bpm/bpm-service/src/main/java/bpm/service/BPMServiceApplication.java
+++ bpm/bpm-service/src/main/java/bpm/service/BPMServiceApplication.java
@@ -1,8 +1,13 @@
 package bpm.service;
 
 import io.github.devbhuwan.microservices.autoconfigure.MicroServiceAutoConfiguration;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,6 +16,19 @@ public class BPMServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BPMServiceApplication.class, args);
+    }
+
+    @Bean
+    public InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+        return () -> {
+            Group group = identityService.newGroup("user");
+            group.setName("users");
+            group.setType("security-role");
+            identityService.saveGroup(group);
+            User admin = identityService.newUser("admin");
+            admin.setPassword("admin");
+            identityService.saveUser(admin);
+        };
     }
 
 }
